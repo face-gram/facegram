50dbb0508c20184d42041c4ad99169e5dbc8188f
SeYeol00 <dmot0126@gmail.com>
2023-03-01 01:53:11 +0900

    Update SecurityConfig.kt

--- src/main/kotlin/com/facegram/facegrambackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/facegram/facegrambackend/config/SecurityConfig.kt
@@ -10,14 +10,11 @@ import com.facegram.facegrambackend.security.oauth2.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.context.WebApplicationContext
 
 @Configuration
 @EnableWebSecurity
@@ -30,14 +27,13 @@ class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
     private val jwtAccessDeniedHandler: JwtAccessDeniedHandler,
-    private val web: WebSecurityCustomizer {
+    private val web: WebSecurityCustomizer) {
 
     @Bean
     fun webSecurityCustomizer(): WebSecurityCustomizer {
         return (web) -> web?.ignoring()?.antMatchers("/h2-console/**","/favicon.ico")
     }
 //        return (web) -> web?.ignoring()?.antMatchers("/h2-console/**","/favicon.ico")
-    }
      @Bean
      fun configure(http: HttpSecurity):SecurityFilterChain {
         http.authorizeHttpRequests()
